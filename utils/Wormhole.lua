d3fe23ad86c5d9367ef6b071037571aa1de22d40
Scott Ingram <github@scottingram.org>
2023-05-25 13:47:10 -0700

    added support for inheriting from other parents

--- utils/Wormhole.lua
+++ utils/Wormhole.lua
@@ -3,10 +3,17 @@
 
 local _, ADDON_VARS = ...
 
-function ADDON_VARS.Wormhole(table)
+function ADDON_VARS.Wormhole(table, parent)
     if not table then
         table = ADDON_VARS
     end
-    setmetatable(table, { __index = _G }) -- inherit all members of the Global namespace
+    if not parent then
+        if table == ADDON_VARS then
+            parent = _G
+        else
+            parent = ADDON_VARS -- can I automatically get the parent to "table" ?
+        end
+    end
+    setmetatable(table, { __index = parent }) -- inherit all members of the Global namespace (or parent, if provided)
     setfenv(2, table) -- the 2 designates the CALLER's env
 end
